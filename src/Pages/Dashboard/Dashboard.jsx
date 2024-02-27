68c231fc45253dee6668c1e5590e7e454a8f7a2f
walter lacoste <wlacoste@outlook.com>
2024-02-27 02:30:42 -0300

    wip: obtengo datos de bd

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import ProductForm from "../../components/InputNuevoGasto";
-import Movimiento from "../../components/InputNuevoGasto/NuevoMovimiento";
+import ProductForm from "src/components/InputNuevoGasto";
+import Movimiento from "src/components/InputNuevoMovimiento";
+import { useFirestore } from "src/hooks/useFirestore";
 
 const Dashboard = () => {
+  const { data, error, loading } = useFirestore();
+
+  if (loading) return <p>Loading data...</p>;
+  if (error) return <p>{error.message}</p>;
+
   return (
     <>
       <div>Dashboard</div>
+      <div>
+        {data.map((item) => (
+          <div key={item.nanoid}>
+            <p>{item.nanoid}</p>
+            <p>{item.origin}</p>
+            <p>{item.uid}</p>
+          </div>
+        ))}
+      </div>
       <ProductForm />
       <Movimiento />
     </>
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, where, query } from "firebase/firestore";
 
 export const useFirestore = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    console.log("getData");
+    getData();
+  }, []);
+
   const getData = async () => {
     try {
       setLoading(true);
-      const querySnapshot = await getDocs(collection(db, "urls"));
+      const dataRef = collection(db, "urls");
+      const q = query(
+        dataRef,
+        where("uid", "==", "P3tXSXLIAzN47tYL5KEFASJktkr2")
+      );
+      const querySnapshot = await getDocs(q);
+      const dataDB = querySnapshot.docs.map((doc) => doc.data());
+      setData(dataDB);
     } catch (error) {
       console.log(error);
       setError(error.message);
@@ -19,5 +31,9 @@ export const useFirestore = () => {
     }
   };
 
-  return {};
+  return {
+    data,
+    error,
+    loading,
+  };
 };
